ada6bfbb8df3ae9fe687ec3affab095a4af66a90
Humberto Leal <humberto.adolfo@hotmail.com>
2017-07-17 17:00:39 -0500

    Codigo que no sirve. Completo.

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,3 @@
-/*
- Basic ESP8266 MQTT example
-
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
-
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
-
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
-
- To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
-
-*/
-
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <Stepper.h>
@@ -42,7 +17,7 @@ int value = 0;
 #define OUTSTEPPER "outStepper"
 
 const int stepsPerRevolution = 200;
-
+const int tip120 = 5;
 
 
 Stepper myStepper(stepsPerRevolution, 13, 12, 14, 16);
@@ -51,11 +26,11 @@ void setup_wifi();
 void callback(char* topic, byte* payload, unsigned int length);
 
 void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1884);
   client.setCallback(callback);
+  pinMode(tip120, OUTPUT);
 
   myStepper.setSpeed(120);
 }
@@ -94,12 +69,12 @@ void callback(char* topic, byte* payload, unsigned int length) {
   if ((char)payload[0] == '+') {
     myStepper.step(stepsPerRevolution);   // Turn the LED on (Note that LOW is the voltage level
     client.publish(OUTSTEPPER, "Vuelta en sentido del reloj.");
-
+    digitalWrite(tip120, HIGH);
+    
   } else if((char)payload[0] == '-'){
     myStepper.step(-stepsPerRevolution);  // Turn the LED off by making the voltage HIGH
-    client.publish(OUTSTEPPER, "Vuelta en sentido contrario al reloj.");
-  }
-
+    client.publish(OUTSTEPPER, "Vuelta en sentido contrario al reloj.");  }
+    digitalWrite(tip120, LOW);
 }
 
 void reconnect() {
@@ -107,7 +82,7 @@ void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("ESP8266Client")) {
+    if (client.connect("Pasito a pasito. Dale suavecito. Ba dum tss.")) {
       Serial.println("connected");
       // Once connected, publish an announcement...
       // ... and resubscribe
@@ -121,8 +96,8 @@ void reconnect() {
     }
   }
 }
-void loop() {
 
+void loop() {
   if (!client.connected()) {
     reconnect();
   }
