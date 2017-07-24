d646b158263adb06705b370df43884f3912c5c02
Humberto <humberto.adolfo@hotmail.com>
2017-07-24 12:08:12 -0500

    Cambio del puerto

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@ void callback(char *topic, byte *payload, unsigned int length);
 void setup() {
   Serial.begin(115200);
   setup_wifi();
-  client.setServer(mqtt_server, 1884);
+  client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
   pinMode(tip120, OUTPUT);
 
